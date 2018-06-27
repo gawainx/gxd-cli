7e294e4cec09f6dfb5d17161fddf0a21094e0ce4
gawainx <liangyixp@live.cn>
2018-06-27 15:17:18 +0800

    use dep

--- MultiCfg.go
+++ MultiCfg.go
@@ -3,33 +3,36 @@
  *Author: Gawain Antarx
  *Create Date: 2018-May-30
  *
-*/
+ */
 
 package main
 
 import (
-    "github.com/BurntSushi/toml"
-    "log"
+	"log"
+
+	"github.com/BurntSushi/toml"
 )
+
+//ContainerConfigs : Multi-Container Configs
 type ContainerConfigs []ContainerConfig
 
 type MultiTOMLConfig struct {
-    Title           string
-    Net             NetworkConfig    `toml:"network"`
-    Services        ContainerConfigs
+	Title    string
+	Net      NetworkConfig `toml:"network"`
+	Services ContainerConfigs
 }
 
 // Init toml from *.toml
-func (m *MultiTOMLConfig) InitFromFile(filename string){
-    _,e := toml.DecodeFile(filename,m)
-    if e != nil{
-        log.Fatal(e)
-    }
+func (m *MultiTOMLConfig) InitFromFile(filename string) {
+	_, e := toml.DecodeFile(filename, m)
+	if e != nil {
+		log.Fatal(e)
+	}
 }
 
-func (m *MultiTOMLConfig) RunContainers(){
-    for serIndex,service := range m.Services{
-        log.Printf("Creating %d service...\n",serIndex)
-        service.RunContainer()
-    }
+func (m *MultiTOMLConfig) RunContainers() {
+	for serIndex, service := range m.Services {
+		log.Printf("Creating %d service...\n", serIndex)
+		service.RunContainer()
+	}
 }
